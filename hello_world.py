0645bf5fd12559a3a674402a1e70a9a3655a0359
Lungsta-sudo <lungadlams@yahoo.com>
2025-08-05 09:10:33 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#get user name
+#print user name
+#get user age
+#print user age
+#print "Hello, World!"
+
+input_name = input ('Lunga')
+print ("Users Name:"+ input_name)
+input_age = input ('37')
+print("Users age:"+input_age)
+
+print("Hello, World!")
